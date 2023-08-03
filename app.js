9bbd4d3f81d70acc701e192efed470598ab4c6ff
shikarinkato <ramanjadaun@gmail.com>
2023-08-04 03:59:20 +0530

    Some Changes in Cors

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ config({
 const server = http.createServer(app);
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
