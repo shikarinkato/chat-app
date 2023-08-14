a740b02e0d349166a0d919eb067577c772a198d2
shikarinkato <ramanjadaun@gmail.com>
2023-08-14 21:22:40 +0530

    RealTime Chat Functionality Created

--- server.js
+++ server.js
@@ -15,6 +15,41 @@ const io = new Server(server, {
   cors: { origin: process.env.FRONTEND_URL },
 });
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New Connection");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined the Room: " + room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+  socket.on("stoptyping", (room) => {
+    socket.in(room).emit("stoptyping");
+  });
+
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log("chat.users is not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user).emit("message received" + newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User Disconnected");
+    socket.leave(userData._Id);
+  });
 });
